23e2a4c22414d205683dcfb39ac038808a3d05ca
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-12-28 23:27:03 +0100

    feat: add component

---
+++ components/theme-changer.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+
+import moonIcon from '../public/icons/moon.svg';
+import sunIcon from '../public/icons/sun.svg';
+
+export default function ThemeChanger() {
+	const [mounted, setMounted] = useState(false);
+	const { theme, setTheme } = useTheme();
+
+	// when mounted on client, now we can show the UI
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) return null;
+
+	const setThemeHandler = () => {
+		setTheme(theme === 'dark' ? 'light' : 'dark');
+	};
+
+	console.log(theme);
+
+	return (
+		<div className="border rounded">
+			<button onClick={setThemeHandler}>
+				{theme === 'light' && <Image src={moonIcon} alt="moon icon" />}
+				{theme === 'dark' && <Image src={sunIcon} alt="sun icon" />}
+			</button>
+		</div>
+	);
+}
