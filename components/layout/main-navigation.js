580cecddcfe91271f0137fecd591602a77ecb15e
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-01-06 18:51:15 +0100

    feat: improved nav items style

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,46 +1,61 @@
-import Image from 'next/image';
+import { useState } from 'react';
 import Link from 'next/link';
 
-import { NavAction, NavToggle } from '../nav-items';
 import ThemeChanger from '../theme-changer';
 
 import Logo from '../logo';
+import ActivityIcon from '../icons/activity-icon';
 
-export default function MainNavigation() {
-	return (
-		<nav className="w-4/5 h-full m-auto flex justify-between items-center">
-			<Link href="/">
-				<a>
-					<Logo />
-				</a>
-			</Link>
+export default function MainNavigation(props) {
+  const [showActivityDropdown, setShowActivityDropdown] = useState(false);
 
-			<div className="h-full flex items-stretch">
-				<ul className="flex mr-16">
-					<li>
-						<NavAction>
-							<Link href="/new">
-								<a>New</a>
-							</Link>
-						</NavAction>
-					</li>
-					<li>
-						<NavAction>
-							<button>Info</button>
-						</NavAction>
-					</li>
-					<li>
-						<NavAction>
-							<button>About</button>
-						</NavAction>
-					</li>
-					<li>
-						<NavToggle>
-							<ThemeChanger />
-						</NavToggle>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  const toggleActiviyDropdown = () => {
+    setShowActivityDropdown((prevState) => !prevState);
+  };
+
+  return (
+    <nav className="w-4/5 h-full m-auto flex items-center justify-between ">
+      <Link href="/">
+        <a>
+          <Logo width={58} height={58} fontSize="1.85rem" />
+        </a>
+      </Link>
+
+      <ul className="h-full text-neutral-400 flex items-stretch">
+        <li className=" w-32 h-full flex items-center justify-center cursor-pointer select-none hover:bg-neutral-900 hover:text-white">
+          <Link href="/new">
+            <a>New</a>
+          </Link>
+        </li>
+        <li
+          className="w-32 h-full flex items-center justify-center cursor-pointer select-none hover:bg-neutral-900 hover:text-white"
+          onClick={props.onSideBarToggle}
+        >
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li className="w-32 h-full flex items-center justify-center transition  select-none relative">
+          <button
+            onClick={toggleActiviyDropdown}
+            className="rounded-full p-4 hover:bg-neutral-900 hover:text-white"
+          >
+            <ActivityIcon size={22} />
+          </button>
+          {showActivityDropdown && (
+            <div className="absolute top-[100%] left-0 bg-neutral-500 w-full text-center">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque
+                esse mollitia temporibus eaque vero eligendi maiores quisquam
+                impedit odio itaque!
+              </p>
+            </div>
+          )}
+        </li>
+        <li className="ml-6 h-full flex items-center justify-center select-none">
+          <ThemeChanger className="rounded-full p-4 hover:bg-neutral-900 hover:text-white" />
+        </li>
+      </ul>
+    </nav>
+  );
 }
