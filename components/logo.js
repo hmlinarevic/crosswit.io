a37d2fd6693101e858aae52536e2b7311fc50f2c
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-01-08 11:30:49 +0100

    feat: replaced width and height prop with size

--- components/logo.js
+++ components/logo.js
@@ -2,22 +2,22 @@ import Image from 'next/image';
 
 import brainPng from '../public/brainv.png';
 
-export default function Logo({ width, height, fontSize }) {
-	return (
-		<div className="flex items-center">
-			<Image
-				src={brainPng}
-				alt="line art logo of human brain"
-				width={width}
-				height={height}
-				objectFit="contain"
-			/>
-			<span
-				className="font-hand uppercase ml-4"
-				style={{ fontSize: `${fontSize}` }}
-			>
-				crossw<span className="font-hand lowercase">i</span>t
-			</span>
-		</div>
-	);
+export default function Logo({ size, fontSize }) {
+  return (
+    <div className="flex items-center">
+      <Image
+        src={brainPng}
+        alt="abstract brain symbol"
+        width={size}
+        height={size}
+        objectFit="contain"
+      />
+      <span
+        className="hidden  font-hand uppercase ml-4 sm:lowercase sm:block"
+        style={{ fontSize: `${fontSize}` }}
+      >
+        crossw<span className="font-hand lowercase">i</span>t
+      </span>
+    </div>
+  );
 }
