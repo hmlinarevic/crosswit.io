a9dce4be1f1d8825a7d536e24569dcd86f30b7a8
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-01-08 19:55:50 +0100

    feat: move icons to svg folder

---
+++ components/icons/svg/activity-icon.js
@@ -0,0 +1,19 @@
+export default function ActivityIcon({ size, className }) {
+  return (
+    <div className={className}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || '24'}
+        height={size || '24'}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
+      </svg>
+    </div>
+  );
+}
