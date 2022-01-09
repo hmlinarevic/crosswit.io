70693e4bd50020c1f27d0e47fc9d3476d565c992
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-01-09 17:49:48 +0100

    feat: add Classnames package

--- components/icons/bar-menu.js
+++ components/icons/bar-menu.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import classNames from 'classnames';
+
 export default function BarMenu({ onToggleMenu, className }) {
   const [animateMenu, setAnimateMenu] = useState(false);
 
@@ -10,38 +12,38 @@ export default function BarMenu({ onToggleMenu, className }) {
 
   const barStyles = {
     1: {
-      backgroundColor: 'white',
       transform: 'rotate(-45deg) translate(-4px, 5px)',
     },
     2: {
       opacity: 0,
     },
     3: {
-      backgroundColor: 'white',
       transform: 'rotate(45deg) translate(-8px, -8px)',
     },
   };
 
+  const barClasses = classNames(
+    'w-[40px] h-[40px] bg-red-200 p-1 cursor-pointer z-50',
+    className
+  );
+
+  console.log(barClasses);
+
   return (
-    <div className={className}>
-      <div
-        onClick={animateMenuHandler}
-        className="w-[50px] h-[50px] bg-black relative m-auto cursor-pointer"
-      >
-        <div className="absolute top-[8px] left-[12px] z-10">
-          <div
-            className="w-[26px] h-[2px] bg-white my-[7px] transition-transform"
-            style={animateMenu ? barStyles[1] : null}
-          />
-          <div
-            className="w-[26px] h-[2px] bg-white my-[7px] transition-transform"
-            style={animateMenu ? barStyles[2] : null}
-          />
-          <div
-            className="w-[26px] h-[2px] bg-white my-[7px] transition-transform"
-            style={animateMenu ? barStyles[3] : null}
-          />
-        </div>
+    <div onClick={animateMenuHandler} className={barClasses}>
+      <div>
+        <div
+          className="w-full h-[2px] bg-neutral-400 my-[7px] transition-transform"
+          style={animateMenu ? barStyles[1] : null}
+        />
+        <div
+          className="w-full h-[2px] bg-neutral-400 my-[7px] transition-transform"
+          style={animateMenu ? barStyles[2] : null}
+        />
+        <div
+          className="w-full h-[2px] bg-neutral-400 my-[7px] transition-transform"
+          style={animateMenu ? barStyles[3] : null}
+        />
       </div>
     </div>
   );
