400c69608b5aaeb2a75d75feefd10f0ef22ac442
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-01-11 22:44:52 +0100

    feat: switch next images with svg components

--- components/theme-changer.js
+++ components/theme-changer.js
@@ -1,29 +1,28 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
-import Image from 'next/image';
 
-import moonIcon from '../public/icons/moon.svg';
-import sunIcon from '../public/icons/sun.svg';
+import MoonIcon from './icons/svg/moon-icon';
+import SunIcon from './icons/svg/sun-icon';
 
-export default function ThemeChanger() {
-	const [mounted, setMounted] = useState(false);
-	const { theme, setTheme } = useTheme();
+export default function ThemeChanger({ className }) {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-	// when mounted on client, now we can show the UI
-	useEffect(() => setMounted(true), []);
+  // when mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
 
-	if (!mounted) return null;
+  if (!mounted) return null;
 
-	const setThemeHandler = () => {
-		setTheme(theme === 'dark' ? 'light' : 'dark');
-	};
+  const setThemeHandler = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
 
-	return (
-		<>
-			<button onClick={setThemeHandler} className="flex">
-				{theme === 'light' && <Image src={moonIcon} alt="moon icon" />}
-				{theme === 'dark' && <Image src={sunIcon} alt="sun icon" />}
-			</button>
-		</>
-	);
+  return (
+    <>
+      <button onClick={setThemeHandler} className={className}>
+        {theme === 'light' && <MoonIcon />}
+        {theme === 'dark' && <SunIcon />}
+      </button>
+    </>
+  );
 }
