3f3c9ae91dc1c565bdde747dd69053a8445839e4
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-04-11 21:20:00 +0200

    feat: add fade-in animation to pages

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,37 @@ import Head from 'next/head';
 
 import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0,
+import { motion } from 'framer-motion';
+
+function MyApp({ Component, pageProps, router }) {
+	return (
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0,
 					maximum-scale=1.0 , user-scalable=no"
-        />
-      </Head>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  );
+				/>
+			</Head>
+			<ThemeProvider attribute="class">
+				<motion.div
+					key={router.route}
+					initial="pageInitial"
+					animate="pageAnimate"
+					variants={{
+						pageInitial: {
+							opacity: 0,
+						},
+						pageAnimate: {
+							opacity: 1,
+						},
+					}}
+				>
+					<Component {...pageProps} />
+				</motion.div>
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default MyApp;
