c3d987f32d65e33648ce75fab976af55cbdf9507
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-04-11 21:17:29 +0200

    feat: add component

--- components/button.js
+++
@@ -1,9 +0,0 @@
-export default function Button({ className, children }) {
-	return (
-		<button
-			className={`${className} w-fit border border-solid border-gray-300 hover:border-gray-400 rounded-xl transition`}
-		>
-			{children}
-		</button>
-	);
-}
