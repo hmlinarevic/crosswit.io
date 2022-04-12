c2dfeb78b89d93ff489d1a250a3ec2873a5de070
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-04-12 23:16:19 +0200

    chore: format

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,13 +1,13 @@
-import clsx from 'clsx';
+import clsx from 'clsx'
 
 export default function Card({ className, children }) {
-	const styles = ``;
+  const styles = ``
 
-	return (
-		<>
-			<div className={clsx('w-full p-10 mb-8 border rounded-xl', className)}>
-				{children}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={clsx('w-full p-10 mb-8 border rounded-xl', className)}>
+        {children}
+      </div>
+    </>
+  )
 }
