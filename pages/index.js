a97d5d64ef13900a3d100d3a7953dddf01aed4fc
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-04-12 23:18:05 +0200

    refactor: remove welcome component abstraction

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-import { useEffect } from 'react';
+import Link from 'next/link'
 
-import Welcome from '../components/welcome';
+import Logo from '../components/logo'
+import Button from '../components/ui/button'
 
 export default function Home(props) {
-	return (
-		<>
-			<Welcome />
-		</>
-	);
+  return (
+    <>
+      <div className="h-screen grid place-content-center">
+        <Logo size={72} fontSize="2.25rem" />
+        <span className="text-center text-lg block opacity-60 mt-[-0.5rem]">
+          {'8-Way Crossword & Memory Trainer'}
+        </span>
+        <Link href="/go">
+          <a>
+            <Button className="min-w-[120px] mt-8">play</Button>
+          </a>
+        </Link>
+        <Link href="/about">
+          <a>
+            <Button className="min-w-[120px]">about</Button>
+          </a>
+        </Link>
+      </div>
+    </>
+  )
 }
