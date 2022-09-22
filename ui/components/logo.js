dbda4bab88e0b7351aecf8958f6b5600988e7387
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-09-22 21:24:03 +0200

    refactor: change how styles are passed to dom element

--- ui/components/logo.js
+++ ui/components/logo.js
@@ -1,29 +1,37 @@
-import Image from 'next/image';
+import Link from 'next/link'
+import Image from 'next/image'
+import clsx from 'clsx'
 
-import classNames from 'classnames';
+import brainPng from '../public/brainv.png'
 
-import brainPng from '../public/brainv.png';
+export default function Logo({
+  className: propStyles,
+  size,
+  textSize,
+  showText,
+}) {
+  return (
+    <div className={clsx('flex items-center justify-center', propStyles)}>
+      <Link href="/">
+        <a>
+          <Image
+            src={brainPng}
+            alt="abstract brain symbol"
+            width={size}
+            height={size}
+            objectFit="contain"
+          />
+        </a>
+      </Link>
 
-const containerStyles = ['flex', 'items-center'];
-
-export default function Logo({ size, fontSize, className: propsStyles }) {
-	const containerClasses = classNames(propsStyles, containerStyles);
-
-	return (
-		<div className={containerClasses}>
-			<Image
-				src={brainPng}
-				alt="abstract brain symbol"
-				width={size}
-				height={size}
-				objectFit="contain"
-			/>
-			<span
-				className="hidden font-hand ml-4 sm:block text-white"
-				style={{ fontSize: `${fontSize}` }}
-			>
-				CROSSWiT
-			</span>
-		</div>
-	);
+      {showText && (
+        <span
+          className="ml-4 font-hand dark:text-white sm:block"
+          style={{ fontSize: `${textSize}` }}
+        >
+          CROSSWiT
+        </span>
+      )}
+    </div>
+  )
 }
