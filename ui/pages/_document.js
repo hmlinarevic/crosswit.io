9814b12e18fc595e305ed02174adfc17511babc8
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-09-23 20:55:50 +0200

    feat: add ubuntu font family

--- ui/pages/_document.js
+++ ui/pages/_document.js
@@ -1,22 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Merriweather&display=swap"
-						rel="stylesheet"
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Merriweather&family=Ubuntu&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default MyDocument;
+export default MyDocument
