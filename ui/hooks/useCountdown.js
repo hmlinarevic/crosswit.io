2464366a79543a924fa68df4ca5f945c4bdc4de3
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-03 23:01:47 +0200

    refactor: change ids variable to timerIds

--- ui/hooks/useCountdown.js
+++ ui/hooks/useCountdown.js
@@ -3,39 +3,38 @@ import { useState, useEffect } from 'react'
 const ONE_SEC = 1000
 
 // clear async timers in useEffect
-const ids = []
-
-const calcTime = (totalSeconds) => {
-  const minutes = Math.floor(totalSeconds / 60)
-  const seconds = totalSeconds % 60
-
-  return { minutes, seconds }
-}
+const timerIds = []
 
 export default function useCountdown(seconds, delayStart, onEnd) {
   const [secondsLeft, setSecondsLeft] = useState(seconds)
 
-  const time = calcTime(secondsLeft)
+  const stopRunningTime = () => {
+    timerIds.forEach((id) => clearInterval(id))
+  }
 
   useEffect(() => {
-    ids[0] = setTimeout(() => {
-      ids[1] = setInterval(() => {
+    timerIds[0] = setTimeout(() => {
+      timerIds[1] = setInterval(() => {
         setSecondsLeft((secondsLeft) => secondsLeft - 1)
       }, ONE_SEC)
     }, delayStart)
 
     return () => {
-      ids.forEach((id) => clearInterval(id))
+      timerIds.forEach((id) => clearInterval(id))
     }
   }, [delayStart])
 
   useEffect(() => {
     if (secondsLeft === 0) {
-      console.log('timer cleared, seconds === 0')
-      clearInterval(ids[1])
+      clearInterval(timerIds[1])
+
+      // prevent running this block again
+      setSecondsLeft(null)
+
+      // call ending function if specified
       onEnd && onEnd()
     }
   }, [secondsLeft, onEnd])
 
-  return [time.minutes, time.seconds]
+  return [secondsLeft, stopRunningTime]
 }
