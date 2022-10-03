f16a8d1d46c1f0e3e73fd0fc674ac9f67fbd74c8
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-03 22:58:50 +0200

    feat: add fa icons

--- ui/components/theme-changer.js
+++ ui/components/theme-changer.js
@@ -1,28 +1,51 @@
-import { useState, useEffect } from 'react';
-import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
 
-import MoonIcon from './icons/svg/moon-icon';
-import SunIcon from './icons/svg/sun-icon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
 
-export default function ThemeChanger({ className }) {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+import MoonIcon from './icons/svg/moon-icon'
+import SunIcon from './icons/svg/sun-icon'
+
+export default function ThemeChanger({ className, children }) {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
 
   // when mounted on client, now we can show the UI
-  useEffect(() => setMounted(true), []);
+  useEffect(() => setMounted(true), [])
 
-  if (!mounted) return null;
+  if (!mounted) return null
 
   const setThemeHandler = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
-  };
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
 
   return (
     <>
       <button onClick={setThemeHandler} className={className}>
-        {theme === 'light' && <MoonIcon />}
-        {theme === 'dark' && <SunIcon />}
+        {theme === 'light' && (
+          <FontAwesomeIcon
+            className="text-neutral-300 transition-colors hover:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300"
+            icon={faMoon}
+            size="lg"
+          />
+        )}
+        {theme === 'dark' && (
+          <FontAwesomeIcon
+            className="text-neutral-300 transition-colors hover:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300"
+            icon={faSun}
+            size="lg"
+          />
+        )}
+        {theme === 'system' && (
+          <FontAwesomeIcon
+            className="text-neutral-300 transition-colors hover:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300"
+            icon={faSun}
+            size="lg"
+          />
+        )}
       </button>
     </>
-  );
+  )
 }
