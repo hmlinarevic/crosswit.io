fdde54d71633a5dad3dc34383d8bdb0ad6d3cce4
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-03 22:55:13 +0200

    feat: move hide/show functionality from useEffect to specific handlers

--- ui/components/game.js
+++ ui/components/game.js
@@ -1,48 +1,93 @@
 import { useState, useEffect, useCallback } from 'react'
-import { useRouter } from 'next/router'
 
 import Fade from './fade'
 import Logo from './logo'
 import Board from './crossword/board'
 import Timer from './timer'
 
-export default function Game({ level, delays, time }) {
+const timerIds = []
+
+export default function Game({ level, delays, time, onGameEnd }) {
+  const findWordsNum = level.insertedWords.length
+
   const [showBoard, setShowBoard] = useState()
   const [showOther, setShowOther] = useState()
-  const [numOfWordsToFind, setNumOfWordsToFind] = useState(
-    level.insertedWords.length
+  const [numOfWordsToFind, setNumOfWordsToFind] = useState(findWordsNum)
+  const [areWordsFound, setAreWordsFound] = useState()
+  const [timeLeft, setTimeLeft] = useState()
+
+  // --- fade in/out components on the page ---
+
+  const hideGameComponents = useCallback(
+    (cb) => {
+      timerIds[0] = setTimeout(() => {
+        setShowOther(false)
+      }, delays.short)
+
+      timerIds[1] = setTimeout(() => {
+        setShowBoard(false)
+      }, delays.long)
+
+      timerIds[2] = setTimeout(() => {
+        cb()
+      }, delays.long + delays.fade)
+    },
+    [delays]
   )
 
-  const router = useRouter()
+  const showGameComponents = useCallback(() => {
+    timerIds[0] = setTimeout(() => {
+      setShowBoard(true)
+    }, delays.short)
 
-  const goToIndexPage = () => {
-    router.push('/')
-  }
+    timerIds[1] = setTimeout(() => {
+      setShowOther(true)
+    }, delays.normal)
+  }, [delays])
 
-  const onFoundWord = useCallback(() => {
+  // --- handlers ---
+
+  const handleFoundWord = useCallback(() => {
     setNumOfWordsToFind((prevNum) => prevNum - 1)
   }, [])
 
-  console.log({ numOfWordsToFind })
+  // when user finds all words
+
+  const handleAllWordsFound = useCallback(() => {
+    hideGameComponents(() =>
+      onGameEnd({ result: 'completed', timeLeft, wordsFoundNum: findWordsNum })
+    )
+  }, [timeLeft, onGameEnd, findWordsNum, hideGameComponents])
+
+  // when timer has no more seconds left
+
+  const handleTimerEnded = () => {
+    hideGameComponents(() => onGameEnd({ result: 'failed' }))
+  }
+
+  // --- effects ---
+
+  // run after inital render
 
   useEffect(() => {
-    const ids = []
-
-    setTimeout(() => {
-      ids[0] = setTimeout(() => {
-        setShowBoard(true)
-      }, delays.short)
-
-      ids[1] = setTimeout(() => {
-        setShowOther(true)
-      }, delays.normal)
-      //
-    }, delays.short)
+    showGameComponents()
 
     return () => {
-      ids.forEach((id) => clearInterval(id))
+      timerIds.forEach((id) => clearInterval(id))
     }
-  }, [delays])
+  }, [showGameComponents])
+
+  // when user finds all words (numOfWordsToFind === 0)
+
+  useEffect(() => {
+    if (!numOfWordsToFind) {
+      setAreWordsFound(true)
+    }
+
+    if (!numOfWordsToFind && timeLeft) {
+      handleAllWordsFound()
+    }
+  }, [numOfWordsToFind, timeLeft, handleAllWordsFound])
 
   return (
     <section className="grid h-screen place-content-center">
@@ -50,14 +95,20 @@ export default function Game({ level, delays, time }) {
         <Logo size={64} className="mx-auto mb-16 w-fit" />
       </Fade>
       <Fade toggler={showBoard} duration={delays.fade} className="self-center">
-        <Board level={level} onFoundWord={onFoundWord} />
+        <Board level={level} onFoundWord={handleFoundWord} />
       </Fade>
       <Fade
         toggler={showOther}
         duration={delays.fade}
         className="mt-16 text-center"
       >
-        <Timer seconds={time.crossword} onEnd={goToIndexPage} />
+        <Timer
+          seconds={time.crossword}
+          delayStart={1000 + delays.fade}
+          onTimeEnd={handleTimerEnded}
+          areWordsFound={areWordsFound}
+          onWordsFoundSetTimeLeft={setTimeLeft}
+        />
       </Fade>
     </section>
   )
