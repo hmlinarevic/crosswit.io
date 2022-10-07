1d31659e28b1c8c61c36cd2e18c3386e0714f6b6
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-07 08:56:20 +0200

    feat: add content and styles

--- ui/pages/about.js
+++ ui/pages/about.js
@@ -1,66 +1,75 @@
-import Link from 'next/link';
+import clsx from 'clsx'
 
-import Card from '../components/ui/card';
-import Logo from '../components/logo';
-import HowToPlay from '../components/about/how-to-play';
-import Scoreboard from '../components/about/scoreboard';
+import Logo from '../components/logo'
 
-const Title = ({ children: title }) => {
-	return <h2 className="text-3xl text-center font-bold mb-2">{title}</h2>;
-};
+const instructions = [
+  'Memorize words.',
+  'Look for memorized words in the puzzle.',
+  'Click and drag to select a word.',
+  'Find all words to score points and to move to the next level.',
+  'Reach and complete level 10.',
+]
 
-const Text = ({ children: text }) => {
-	return <p className="text-center opacity-70">{text}</p>;
-};
+const InstructionStep = ({ step: number, description, isLastStep }) => {
+  const borderColor = isLastStep ? 'border-[#EB5390]' : 'border-[#57489D]'
 
-const Nav = () => {
-	return (
-		<nav className="border-b border-neutral-700 ">
-			<div className="max-w-[1200px] py-2 mx-auto flex justify-between items-center">
-				<Logo size={42} fontSize="1.35rem" />
-				<ul className="text-lg font-light flex">
-					<li className="mr-20">
-						<Link href="/">
-							<a>go back</a>
-						</Link>
-					</li>
-					<li>
-						<Link href="/">
-							<a>change theme</a>
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
-};
+  return (
+    <div className=" mb-8 last:mb-0">
+      {/* bulleted number */}
+      <span
+        className={clsx(
+          'mx-auto my-1 flex h-[30px] w-[30px] items-center justify-center rounded-full border',
+          borderColor
+        )}
+      >
+        {number}
+      </span>
+      {/* instructions */}
+      <p className="mx-auto max-w-[300px] text-center text-neutral-500">
+        {description}
+      </p>
+    </div>
+  )
+}
 
-export default function About(second) {
-	return (
-		<>
-			<Nav />
-			<div className="max-w-[1200px] mx-auto pt-10 grid grid-cols-2 gap-x-32">
-				<div className="flex flex-col ">
-					<Card className="bg-violet-700/5 border-violet-400/60">
-						<Title>Welcome</Title>
-						<Text>Thank you for using the app!</Text>
-					</Card>
-					<Card className="bg-pink-700/5 border-pink-400/60">
-						<Title>Did you know?</Title>
-						<Text>
-							Crosswords alleviate anxiety, which will improve your mood.
-						</Text>
-					</Card>
-					<Card className="h-full bg-lime-700/5 border-lime-400/60 mb-0 grid place-content-center">
-						<Title>Your Scoreboard</Title>
-						<Text>Complete level 10</Text>
-					</Card>
-				</div>
-				<Card className="bg-violet-700/5 border-violet-400/60 mb-0">
-					<Title>How to play</Title>
-					<HowToPlay />
-				</Card>
-			</div>
-		</>
-	);
+export default function About() {
+  return (
+    <section className="h-screen pt-24 text-center font-ubuntu">
+      <Logo isLink size={72} showText={true} textSize="2.5rem" />
+
+      <h2 className="mt-12 mb-2 text-3xl">Welcome</h2>
+      <p className="mx-auto max-w-[500px] text-neutral-500">
+        Thank you for trying out the app!
+      </p>
+
+      <h2 className="mt-12 mb-2 text-3xl">About</h2>
+      <p className="mx-auto max-w-[500px] text-neutral-500">
+        Crosswit is a game where you can exercise parts of your brain
+        responsible for short term memory.
+      </p>
+      <p className="mx-auto mt-4 max-w-[500px] text-neutral-500">
+        Playing the game without writing the words down or taking screenshots
+        will have the best effect for this type of exercise.
+      </p>
+
+      <h2 className="mt-12 mb-2 text-3xl">Did You Know?</h2>
+      <p className="mx-auto max-w-[500px] text-neutral-500">
+        Crosswords alleviate anxiety, which will improve your mood.
+      </p>
+
+      <h2 className="mt-12 mb-6 text-3xl">How To Play?</h2>
+      {instructions.map((instruction, i, ar) => {
+        // const isLastStep = Boolean(i === ar.length - 1)
+
+        return (
+          <InstructionStep
+            key={`i-s${i + 1}`}
+            step={i + 1}
+            description={instruction}
+            // isLastStep={isLastStep}
+          />
+        )
+      })}
+    </section>
+  )
 }
