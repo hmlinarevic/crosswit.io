8b097f35d066d2f0f1f80d15304815a50de3b6d6
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-14 19:23:34 +0200

    feat: modify how timer is stopped and time remaining is calculated

--- ui/components/timer.js
+++ ui/components/timer.js
@@ -1,15 +1,51 @@
+import { useEffect } from 'react'
+
+import clsx from 'clsx'
+
 import useCountdown from '../hooks/useCountdown'
 
 const formatTime = (min, sec) =>
   `${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}`
 
+const calcTime = (totalSeconds) => {
+  const minutes = Math.floor(totalSeconds / 60)
+  const seconds = totalSeconds % 60
+
+  return { minutes, seconds }
+}
+
 export default function Timer({
   className,
   seconds: totalSeconds,
   delayStart = 0,
-  onEnd,
+  onTimeEnd,
+  areWordsFound,
+  onWordsFoundSetTimeLeft,
 }) {
-  const [minutes, seconds] = useCountdown(totalSeconds, delayStart, onEnd)
+  const [secondsLeft, stopRunningTime] = useCountdown(
+    totalSeconds,
+    delayStart,
+    onTimeEnd
+  )
 
-  return <span className={className}>{formatTime(minutes, seconds)}</span>
+  const time = calcTime(secondsLeft)
+
+  useEffect(() => {
+    if (areWordsFound) {
+      stopRunningTime()
+      onWordsFoundSetTimeLeft(secondsLeft)
+    }
+    return () => {
+      stopRunningTime()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [areWordsFound])
+
+  return (
+    <span
+      className={clsx('font-ubuntuMono text-xl text-neutral-700', className)}
+    >
+      {formatTime(time.minutes, time.seconds)}
+    </span>
+  )
 }
