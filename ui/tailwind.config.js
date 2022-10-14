679f5fb4793f39fd212b963854c0a829ad81f654
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-14 19:25:28 +0200

    feat: add different font families

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,16 +1,21 @@
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
-	darkMode: 'class',
-	theme: {
-		fontFamily: {
-			puzzle: ['Merriweather'],
-			hand: ['Architects Daughter'],
-		},
-		extend: {},
-	},
-	variants: {},
-	plugins: [],
-};
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      merriweather: ['Merriweather'],
+      hand: ['Architects Daughter'],
+      ubuntu: ['Ubuntu'],
+      ubuntuMono: ['Ubuntu Mono'],
+      sourceCodePro: ['Source Code Pro'],
+      titilliumWeb: ['Titillium Web'],
+      roboto: ['Roboto'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
