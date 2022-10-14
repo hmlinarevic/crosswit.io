1807b285c94f22ec852a46408cdb2828a6cbb28b
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-14 18:49:01 +0200

    feat: add component

---
+++ ui/components/leaderboard.js
@@ -0,0 +1,10 @@
+import Modal from './ui/modal'
+
+export default function Leaderboard({ onClose }) {
+  return (
+    <Modal onClose={onClose}>
+      <h2>Leaderboard</h2>
+      <p>coming soon...</p>
+    </Modal>
+  )
+}
