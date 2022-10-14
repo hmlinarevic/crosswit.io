129bdcb4c0579dcbceefc704c29545f46e1c2cbd
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-14 19:04:16 +0200

    refactor: modify data fetching and how state is used in the component

--- ui/pages/play.js
+++ ui/pages/play.js
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
-import Fade from '../components/fade'
-import Prologue from '../components/prologue'
+import { fetchAllCrosswordLevels, calcGameScore } from '../utils'
+
+import Memorize from '../components/memorize'
 import Game from '../components/game'
+import GameEnd from '../components/game-end/'
 
 const DELAYS = {
   // in miliseconds
-  prologue: {
+  memorize: {
     firstPart: 4000,
     secondPart: 4000,
   },
@@ -16,90 +18,159 @@ const DELAYS = {
   long: 1000,
 }
 
-const fetchAllLevels = async () => {
-  const levels = {}
-  let res, data, test
+let crossword
 
-  for (let i = 1; i <= 10; i++) {
-    if (i === 4) {
-      test = true
-    }
-    try {
-      res = await fetch(
-        `http://localhost:8080/api/crossword/${!test ? i : test}`
-      )
-      if (res.ok) {
-        data = await res.json()
+export default function Play({ allCrosswordLevels }) {
+  const [showUi, setShowUi] = useState({
+    isMemorizeNext: true,
+    isGameNext: false,
+    isGameDone: false,
+  })
+
+  const [userStats, setUserStats] = useState({
+    scores: {
+      level: 0,
+      total: 0,
+    },
+  })
+
+  const [gameStats, setGameStats] = useState({
+    level: 1,
+    result: null,
+    timeLeft: null,
+    wordsFoundNum: null,
+  })
+
+  const [retryData, setRetryData] = useState()
+
+  if (retryData) {
+    crossword = retryData.crossword
+  } else {
+    crossword = allCrosswordLevels[gameStats.level]
+  }
+
+  // --- handlers ---
+
+  const handleMemorizeEnd = () => {
+    setShowUi((ui) => {
+      return {
+        ...ui,
+        isMemorizeNext: false,
+        isGameNext: true,
       }
-    } catch (error) {
-      console.log('error')
+    })
+  }
+
+  const handleGameEnd = useCallback((data) => {
+    setGameStats((gameStats) => {
+      return {
+        ...gameStats,
+        result: data.result,
+        timeLeft: data.timeLeft,
+        wordsFoundNum: data.wordsFoundNum,
+      }
+    })
+
+    if (data.result === 'completed') {
+      const score = calcGameScore(data.wordsFoundNum, data.timeLeft)
+
+      setUserStats((userStats) => {
+        return {
+          ...userStats,
+          scores: {
+            level: score,
+            total: userStats.scores.total + score,
+          },
+        }
+      })
+
+      setRetryData(null)
     }
-    levels[i] = data
+
+    setShowUi((ui) => {
+      return {
+        ...ui,
+        isGameDone: true,
+        isGameNext: false,
+      }
+    })
+  }, [])
+
+  const handleNextClick = () => {
+    // update level
+    setGameStats((gameStats) => {
+      return {
+        ...gameStats,
+        level: gameStats.level + 1,
+      }
+    })
+
+    // mount components
+    setShowUi((ui) => {
+      return {
+        ...ui,
+        isMemorizeNext: true,
+        isGameDone: false,
+      }
+    })
   }
 
-  return levels
-}
+  const handleRetryGame = (data) => {
+    setRetryData({ crossword: data })
 
-export default function Play({ level, time, allLevels }) {
-  // move to Game immediately with true
-  const [isPrologueDone, setIsPrologueDone] = useState(true)
-  const [userLevel, setUserLevel] = useState(1)
-
-  const currentLevel = allLevels[userLevel]
-
-  console.log({ currentLevel })
-
-  const wordsToMemorize = level.insertedWords.map((data) => data.word)
-
-  console.log({ allLevels })
-  const handlePrologueEnd = () => {
-    setIsPrologueDone(true)
+    setShowUi((ui) => {
+      return {
+        ...ui,
+        isMemorizeNext: true,
+        isGameNext: false,
+        isGameDone: false,
+      }
+    })
   }
-
-  useEffect(() => {}, [])
 
   return (
     <>
-      {!isPrologueDone && (
-        <Prologue
-          words={wordsToMemorize}
-          onEnd={handlePrologueEnd}
+      {showUi.isMemorizeNext && (
+        <Memorize
+          level={gameStats.level}
+          wordsToMemorize={crossword.insertedWords.map((data) => data.word)}
+          timeToMemorize={crossword.timeAllocation.memorize}
+          onEnd={handleMemorizeEnd}
           delays={DELAYS}
-          time={currentLevel.time}
         />
       )}
 
-      {isPrologueDone && (
+      {showUi.isGameNext && (
         <Game
-          level={currentLevel.level}
+          crossword={crossword}
+          timeToPlay={crossword.timeAllocation.game}
           delays={DELAYS}
-          time={currentLevel.time}
+          onGameEnd={handleGameEnd}
+        />
+      )}
+
+      {showUi.isGameDone && (
+        <GameEnd
+          level={gameStats.level}
+          result={gameStats.result}
+          timeLeft={gameStats.timeLeft}
+          wordsFoundNum={gameStats.wordsFoundNum}
+          levelScore={userStats.scores.level}
+          totalScore={userStats.scores.total}
+          onNextClick={handleNextClick}
+          onRetryClick={handleRetryGame}
         />
       )}
     </>
   )
 }
 
-export async function getStaticProps() {
-  let data
-
-  try {
-    const res = await fetch(
-      // 'https://limitless-sea-13267.herokuapp.com/api/crossword/3'
-      'http://localhost:8080/api/crossword/3'
-    )
-    data = await res.json()
-  } catch (err) {
-    console.log(err)
-  }
-
-  const levels = await fetchAllLevels()
+export async function getServerSideProps() {
+  const data = await fetchAllCrosswordLevels()
 
   return {
     props: {
-      level: data.level,
-      time: data.time,
-      allLevels: levels,
+      allCrosswordLevels: data,
     },
   }
 }
