47e3656bd7ebef0816ce7ae4e5ccfc5970dd0aad
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-14 19:16:39 +0200

    refactor: modify props being passed to children

--- ui/components/game-end/index.js
+++ ui/components/game-end/index.js
@@ -1,28 +1,49 @@
+import { useRouter } from 'next/router'
+
 import GameEndCompleted from './game-end-completed'
 import GameEndFailed from './game-end-failed'
 
 export default function GameEnd({
-  scores,
   level,
   result,
+  levelScore,
+  totalScore,
   timeLeft,
   wordsFoundNum,
   onNextClick,
+  onRetryClick,
 }) {
+  const router = useRouter()
+
+  const handleQuitClick = () => {
+    router.push('/')
+  }
+
   return (
     <div className="relative grid h-screen place-content-center justify-items-center font-ubuntuMono">
       {result === 'completed' && (
         <GameEndCompleted
-          scores={scores}
           level={level}
           result={result}
+          levelScore={levelScore}
+          totalScore={totalScore}
           timeLeft={timeLeft}
           wordsFoundNum={wordsFoundNum}
           onNextClick={onNextClick}
+          onQuitClick={handleQuitClick}
         />
       )}
 
-      {result === 'failed' && <GameEndFailed level={level} result={result} />}
+      {result === 'failed' && (
+        <GameEndFailed
+          level={level}
+          result={result}
+          levelScore={levelScore}
+          totalScore={totalScore}
+          onQuitClick={handleQuitClick}
+          onRetryClick={onRetryClick}
+        />
+      )}
     </div>
   )
 }
