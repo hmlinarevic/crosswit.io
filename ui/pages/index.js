b561b6ca72d68b8d2c8362c40e16c9ee1f67e1fd
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-10-14 19:15:31 +0200

    feat: add leaderboard

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,19 +1,23 @@
 import { useState } from 'react'
 
 import Link from 'next/link'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCrown } from '@fortawesome/free-solid-svg-icons'
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 
+import brainPng from '../public/brainv.png'
+
 import Fade from '../components/fade'
-import Logo from '../components/logo'
 import Button from '../components/ui/button'
 import ThemeChanger from '../components/theme-changer'
+import Leaderboard from '../components/leaderboard'
 
 export default function Home() {
   const [showContent, setShowContent] = useState(true)
+  const [showLeaderboard, setShowLeaderboard] = useState(false)
 
   const router = useRouter()
 
@@ -25,48 +29,65 @@ export default function Home() {
     router.push('/play')
   }
 
-  return (
-    <section className="grid h-screen place-content-center">
-      <Fade toggler={showContent} duration={500} onEnd={changePage}>
-        <Logo size={72} showText={true} textSize="2.5rem" />
+  const showLeaderboardHandler = () => {
+    setShowLeaderboard((prevState) => !prevState)
+  }
 
-        <span className="mt-[-12px] block text-center font-ubuntu text-lg opacity-60">
-          {'8-Way Crossword & Memory Trainer'}
-        </span>
-        <Button className="mt-6 py-2 px-8" onClick={handlePlayClick}>
-          play
-        </Button>
-        <div className="mx-auto mt-12 flex w-[120px] items-center justify-evenly text-center">
-          {/* leaderboard */}
-          <button>
-            <Link href="/">
-              <a>
-                <FontAwesomeIcon
-                  icon={faCrown}
-                  size="lg"
-                  className="text-neutral-300 transition-colors hover:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300"
-                />
-              </a>
-            </Link>
-          </button>
-          {/* about */}
-          <button>
-            <Link href="/about">
-              <a>
-                <FontAwesomeIcon
-                  icon={faCircleInfo}
-                  size="lg"
-                  className="text-neutral-300 transition-colors hover:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300"
-                />
-              </a>
-            </Link>
-          </button>
-          {/* themes */}
-          <div>
-            <ThemeChanger />
+  return (
+    <>
+      {showLeaderboard && <Leaderboard onClose={showLeaderboardHandler} />}
+
+      <section className="grid h-screen place-content-center">
+        <Fade toggler={showContent} duration={500} onEnd={changePage}>
+          {/* logo */}
+          <div className="flex select-none justify-center font-titilliumWeb text-6xl">
+            <span>cr</span>
+            <div className="relative top-[10px]">
+              <Image
+                src={brainPng}
+                alt="abstract brain symbol"
+                width={60}
+                height={60}
+                objectFit="contain"
+              />
+            </div>
+            <span>sswit</span>
           </div>
-        </div>
-      </Fade>
-    </section>
+
+          <span className="mt-[-6px] block text-center font-ubuntu text-lg opacity-60">
+            {'word search & memory trainer'}
+          </span>
+          <Button className="mt-6 py-2 px-8" onClick={handlePlayClick}>
+            play
+          </Button>
+          <div className="mx-auto mt-12 flex w-[120px] items-center justify-evenly text-center">
+            {/* leaderboard */}
+            <button onClick={showLeaderboardHandler}>
+              <FontAwesomeIcon
+                icon={faCrown}
+                size="lg"
+                className="text-neutral-300 transition-colors hover:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300"
+              />
+            </button>
+            {/* about */}
+            <button>
+              <Link href="/about">
+                <a>
+                  <FontAwesomeIcon
+                    icon={faCircleInfo}
+                    size="lg"
+                    className="text-neutral-300 transition-colors hover:text-neutral-600 dark:text-neutral-600 dark:hover:text-neutral-300"
+                  />
+                </a>
+              </Link>
+            </button>
+            {/* themes */}
+            <div>
+              <ThemeChanger />
+            </div>
+          </div>
+        </Fade>
+      </section>
+    </>
   )
 }
