f586a634c25863c3ee941a576cc08da10e6eb131
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-12-04 19:33:30 +0100

    fix: button hover text

--- components/ui/button.js
+++ components/ui/button.js
@@ -5,7 +5,7 @@ export default function Button({ className: propClasses, children, onClick }) {
         <button
             onClick={onClick}
             className={clsx(
-                "block min-h-[32px] min-w-[96px] rounded-lg border-2 border-rose border-opacity-10 bg-rose bg-opacity-10 font-roboto text-sm tracking-wide text-rose hover:border-rose hover:font-bold hover:bg-rose hover:bg-opacity-80 hover:text-base",
+                "block min-h-[32px] min-w-[96px] rounded-lg border-2 border-rose border-opacity-10 bg-rose bg-opacity-10 font-roboto text-sm tracking-wide text-rose hover:border-rose hover:font-bold hover:bg-rose hover:bg-opacity-80 hover:text-black",
                 propClasses
             )}
         >
